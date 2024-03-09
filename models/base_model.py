592e2a6b39f699ce6769c5e15b57b4ad91345dc0
Coll76 <collins.kubu@gmail.com>
2024-03-09 14:05:06 -0800

    Store first object

--- models/base_model.py
+++ models/base_model.py
@@ -1,4 +1,11 @@
 #!/usr/bin/python3
+"""
+models/base_model.py
+
+This module defines the BaseModel class that serves as the base class
+from which other classes inherit. It provides a UUID for each instance
+and includes common attributes and methods.
+"""
 
 import uuid
 import cmd
@@ -17,9 +24,8 @@ class BaseModel:
         my_number: A class attribute with a default value of 8
         name: A class attribute with a default value of "Model"
     """
-
-    my_number = 8
-    name = "Model"
+    name = "My_First_Model"
+    my_number = 89
 
     def __init__(self, *args, **kwargs):
         """
@@ -39,7 +45,7 @@ class BaseModel:
         if kwargs:
             for key, value in kwargs.items():
                 # Exclude '__class__'
-                if key not in ['__class__', 'created_at' 'updated_at']:
+                if key not in ['__class__', 'created_at', 'updated_at']:
                     setattr(self, key, value)
 
             if 'created_at' in kwargs:
@@ -50,11 +56,18 @@ class BaseModel:
                 self.updated_at = datetime.strptime(
                         kwargs['updated_at'], '%Y-%m-%dT%H:%M:%S.%f'
                         )
+            if 'id' not in kwargs:
+                self.id = str(uuid.uuid4())
+                self.created_at = datetime.now()
+                self.updated_at = datetime.now()
+                storage.new(self)
+                storage.save()
 
-        self.id = str(uuid.uuid4())
-        self.created_at = datetime.now()
-        self.updated_at = datetime.now()
-        storage.new(self)
+        else:
+            self.id = str(uuid.uuid4())
+            self.created_at = datetime.now()
+            self.updated_at = datetime.now()
+            storage.new(self)
 
     def __str__(self):
         """
@@ -68,6 +81,7 @@ class BaseModel:
         """
         self.updated_at = datetime.now()
         storage.save()
+        
 
     def to_dict(self):
         """
